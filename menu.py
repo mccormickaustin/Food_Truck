b23fc2c0ce07f304c11331f53ccd182bc93f0eef
mccormickaustin <austinmccormick@mac.lan>
2024-11-19 21:45:57 -0500

    removed an null folder.

--- menu.py
+++ menu.py
@@ -52,7 +52,12 @@ menu = {
 
 # 1. Set up order list. Order list will store a list of dictionaries for
 # menu item name, item price, and quantity ordered
-
+order_list = []
+order_item= {
+        "Item name": item_name,
+        "Price": price,
+        "Quantity": quantity
+    }
 
 # Launch the store and present a greeting to the customer
 print("Welcome to the variety food truck.")
